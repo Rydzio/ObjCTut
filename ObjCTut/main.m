5b675113c196190c5ea52a398bc4f6606774561f
rytych <michal.rytych@geminibs.com>
2022-04-19 21:26:39 +0200

    arrays

--- ObjCTut/main.m
+++ ObjCTut/main.m
@@ -28,7 +28,7 @@ int main(int argc, const char * argv[]) {
         NSString *myName = [NSString stringWithFormat:@"%s", name];
         NSLog(@"My name is %@\n", myName);
         
-    // boolians
+    // booleans
         BOOL isStringEqual = [myName isEqualToString: @"Michal"];
         printf("Are strings equal: %d\n", isStringEqual);
         
@@ -53,22 +53,62 @@ int main(int argc, const char * argv[]) {
     }
     
     // mutable strings
-    // create
+        // create
     NSMutableString *groceryList = [NSMutableString stringWithCapacity:50];
     [groceryList appendFormat:@"%s", "Potato, Banana, Pasta"];
     NSLog(@"groceryList: %@", groceryList);
     
-    // delete
+        // delete
     [groceryList deleteCharactersInRange:NSMakeRange(0, 8)];
     NSLog(@"groceryList: %@", groceryList);
     
-    // insert
+        // insert
     [groceryList insertString:@", Apple" atIndex:13];
     NSLog(@"groceryList: %@", groceryList);
     
-    // replace
+        // replace
     [groceryList replaceCharactersInRange:NSMakeRange(15, 5) withString:@"Orange"];
     NSLog(@"groceryList: %@", groceryList);
+    
+    // arrays
+        // create array
+    NSArray *officeSupplies = @[@"Pencils", @"Paper"];
+    NSLog(@"First : %@", officeSupplies[0]);
+    NSLog(@"Office Supples : %@", officeSupplies);
+    
+        // if array contains items
+    BOOL containsItem = [officeSupplies containsObject:@"Pencils"];
+    NSLog(@"Has Pencils : %d", containsItem);
+    
+        // number of items
+    NSLog(@"Total: %d", (int) [officeSupplies count]);
+    
+        // get index of
+    NSLog(@"Index of pencils is at %lu", (unsigned long) [officeSupplies indexOfObject:@"Pencils"]);
+    
+        // mutable array
+    NSMutableArray *heroes = [NSMutableArray arrayWithCapacity:5];
+        
+        // add objects to mutable array
+    [heroes addObject:@"Batman"];
+    [heroes addObject:@"Flash"];
+    [heroes addObject:@"Wonder Woman"];
+    [heroes addObject:@"Kid Flash"];
+    NSLog(@"%@", heroes);
+    
+        // insert object to mutable array at index
+    [heroes insertObject:@"Superman" atIndex:2];
+    NSLog(@"%@", heroes);
+    
+        // remove
+    [heroes removeObject:@"Flash"]; // by object
+    [heroes removeObjectAtIndex:0]; // by index
+    [heroes removeObjectIdenticalTo:@"Superman" inRange:NSMakeRange(0, 1)]; // by range
+    
+        // iterate through the array
+    for (int i = 0; i < [heroes count]; i++) {
+        NSLog(@"%@", heroes[i]);
+    }
     
     return 0;
 }
