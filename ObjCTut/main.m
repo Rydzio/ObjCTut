f2f41cc668affee77f87b4615e5d6e416ddb7cdb
rytych <michal.rytych@geminibs.com>
2022-04-19 19:43:52 +0200

    Initial Commit

---
+++ ObjCTut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCTut
+//
+//  Created by Michał Rytych on 19/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
